ce8539d9a8bd5dbf3ee95f365efa45a932b79f4f
bhautik@vekariya.in <vbhautik41@gmail.com>
2024-09-26 10:28:43 +0530

    change some

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light bg-light fixed-top">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light bg-light fixed-top me-2">
         <div class="container-fluid">
           <Link
             style={{
